f07a1fca173b7fe3425b725bf696d4fd324f31d7
reda-guemry <guemryreda@gmail.com>
2026-03-11 03:02:41 +0000

    add the crud of categorie

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategorUpdateyRequest;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::withCount('books')->get() ;
+        return response()->json($categories) ;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        $category = Category::create([
+            'name' => $request->name , 
+        ]) ;
+
+        return response()->json([
+            'message' => 'Category created successfully' ,
+            'category' => $category ,
+        ] , 201) ;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return response()->json([$category] , 200) ;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategorUpdateyRequest $request, Category $category)
+    {
+        $category->update([
+            'name' => $request->name ,
+        ]) ;
+
+        return response()->json([
+            'message' => 'Category updated successfully' ,
+            'category' => $category ,
+        ] , 200) ;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete() ;
+
+        return response()->json([
+            'message' => 'Category deleted successfully' ,
+        ] , 200) ;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,18 +10,25 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::post('/register', [AuthController::class, 'register']) ;
-Route::post('/login', [AuthController::class, 'login']) ;
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    
-    Route::post('/logout' , [AuthController::class, 'logout']) ;
 
-    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('categories', CategoryController::class)->only(['index' , 'show'])  ;
 
+
+
+
+    Route::middleware('is_admin')->group(function () {
+        Route::apiResource('categories', CategoryController::class) ->except(['index' , 'show']) ;
+    });
 
 });
+
+
 
 
